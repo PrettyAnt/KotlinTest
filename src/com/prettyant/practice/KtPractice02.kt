2c2744d154c467c1f897fb2d2cc1f0a5780f25a1
prettyant <prettyant@qq.com>
2023-06-16 17:59:37 +0800

    kt进度更新

--- src/com/prettyant/practice/KtPractice02.kt
+++ src/com/prettyant/practice/KtPractice02.kt
@@ -8,16 +8,29 @@ package com.prettyant.practice
  * describle :
  */
 data class UserInfo(var age: Int, var name: String)
+data class StudentInfo(var age: Int, var name: String)
 
 fun main() {
-    println(test2(true, UserInfo(150,"chenyu")) { a: UserInfo ->
-        "输出结果为:$a"
+//    println(test1(true, UserInfo(150, "chenyu")) { a: UserInfo ->
+//        "输出结果为:$a"
+//    })
+
+    println(changeBean(true, UserInfo(728, "chenfei")) {
+        StudentInfo(it.age, it.name)
     })
 }
 
-fun test2(open: Boolean,userInfo: UserInfo, params: (UserInfo) -> String) :String{
+fun test1(open: Boolean, userInfo: UserInfo, params: (UserInfo) -> String): String {
     val any = params(userInfo).takeIf {
         open
     } ?: "不满足takIf里面的条件啊.."
     return any
+}
+
+fun changeBean(open: Boolean, userInfo: UserInfo, params: (UserInfo) -> StudentInfo): Any {
+    val studentInfo = params(userInfo).takeIf {
+        open
+    } ?: "不满足takeIf里面的条件哦 ........."
+    return studentInfo
+
 }
